1d3d8fa23c56d1964fdca621d32e768ebb7e7e59
Omar Ahmed <myemail@email.com>
2019-03-21 06:40:30 +0300

    Modify the WordAdapter Class

--- app/src/main/java/com/example/miwok/WordAdapter.java
+++ app/src/main/java/com/example/miwok/WordAdapter.java
@@ -52,6 +52,11 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // set this text on the number TextView
         defaultTextView.setText(currentWord.getDefaultTranslation());
 
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.image);
+        // Get the image resource ID from the current AndroidFlavor object and
+        // set the image to iconView
+        iconView.setImageResource(currentWord.getImageResourceId());
+
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
